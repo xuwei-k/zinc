d13435665cea8831f7732b859276ef7156aa79f1
xuwei-k <6b656e6a69@gmail.com>
2019-05-06 15:17:29 +0900

    fix procedure syntax
    procedure syntax is deprecated https://github.com/scala/scala/commit/1d4d901fcea0a162cd139836b246e85cc0130a6b

--- zinc/src/sbt-test/apiinfo/main-discovery/src/main/scala/Hello.scala
+++ zinc/src/sbt-test/apiinfo/main-discovery/src/main/scala/Hello.scala
@@ -1,13 +1,13 @@
 package runscala
 
 object MainScala {
-	def main(args: Array[String]) {}
+	def main(args: Array[String]): Unit = {}
 
   object StaticInner {
-	  def main(args: Array[String]) {}
+	  def main(args: Array[String]): Unit = {}
   }
 }
 
 class NoMainScala {
-	def main(args: Array[String]) {}
+	def main(args: Array[String]): Unit = {}
 }
